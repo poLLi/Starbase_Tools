9d93c0999ad65265abfe180c0f76b820884bd7f9
PoLLi <1657076+poLLi@users.noreply.github.com>
2021-08-22 12:37:43 +0200

    update 0.0.7
    added pt-BT localization
    fixed misspellings
    added yolol page
    added weight tooltip
    version bump

---
+++ pages/yolol.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="full-height">
+        <b-container>
+            <div class="mb-5">
+                <h2 class="title text-primary">Yolol Codes</h2>
+                <p class="desc pl-4">Collection of usefull yolol codes and debug/dev tools</p>
+            </div>
+
+            <div class="info">
+                <h2 class="title text-monospace">
+                    <span class="text-primary">Sorry!</span>
+                    <br />
+                    Not done yet :)
+                </h2>
+            </div>
+        </b-container>
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.info {
+    margin-top: 20px;
+}
+</style>
